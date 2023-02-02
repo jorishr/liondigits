1eb6df93741f2304e4cab214e751ac564e9b5688
jorishr <joris@liondigits.com>
2023-02-02 11:15:39 +0100

    Moved: Calender Booking App is now pop-up

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,38 @@
-//page-loader code than only runs on calendar page
-if(document.querySelector('body').classList.contains('calendar-widget')){
-    document.onreadystatechange = function() {
-        if (document.readyState !== "complete"){
-            document.querySelector('.calendly-inline-widget').style.visibility = 'hidden';
-        } else {
-            document.querySelector('.loader').style.visibility = 'hidden';
-            document.querySelector('.calendly-inline-widget').style.visibility = 'visible';
-        }
-    }
-}
+(function (C, A, L) {
+  let p = function (a, ar) {
+    a.q.push(ar);
+  };
+  let d = C.document;
+  C.Cal =
+    C.Cal ||
+    function () {
+      let cal = C.Cal;
+      let ar = arguments;
+      if (!cal.loaded) {
+        cal.ns = {};
+        cal.q = cal.q || [];
+        d.head.appendChild(d.createElement("script")).src = A;
+        cal.loaded = true;
+      }
+      if (ar[0] === L) {
+        const api = function () {
+          p(api, arguments);
+        };
+        const namespace = ar[1];
+        api.q = api.q || [];
+        typeof namespace === "string"
+          ? (cal.ns[namespace] = api) && p(api, ar)
+          : p(cal, ar);
+        return;
+      }
+      p(cal, ar);
+    };
+})(window, "https://app.cal.com/embed/embed.js", "init");
+Cal("init", { origin: "https://app.cal.com" });
+
+// Important: Make sure to add `data-cal-link="liondigits/20min"` attribute to the element you want to open Cal on click
+Cal("ui", {
+  theme: "dark",
+  styles: { branding: { brandColor: "#003F8C" } },
+  hideEventTypeDetails: false,
+});
