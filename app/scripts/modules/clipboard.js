d3a6408ce02cf57cd466aeb2039bfa28a980da9e
jorishr <joris@liondigits.com>
2023-02-03 12:05:50 +0100

    Added: copy confirm text displayed on copyToClipboard

--- app/scripts/modules/clipboard.js
+++ app/scripts/modules/clipboard.js
@@ -4,18 +4,25 @@ Copy PGP key to clipboard
 #########################  
 */
 export function clipboardCopy() {
-  const copyBtn = document.getElementById("pgp-btn-copy");
+  const copyBtn = document.getElementsByClassName("js-pgp-btn-copy")[0];
   if (copyBtn) {
     copyBtn.addEventListener("click", function () {
       const str = document.getElementById("pgp-txt").textContent;
       const textArea = document.createElement("textarea");
+
       textArea.style.position = "absolute";
       textArea.style.top = "-9999px";
       textArea.value = str.slice(5);
       document.body.appendChild(textArea);
+
       textArea.select();
       document.execCommand("copy");
       document.body.removeChild(textArea);
+
+      copyBtn.classList.add("js-copied");
+      setTimeout(function () {
+        copyBtn.classList.remove("js-copied");
+      }, 2000);
     });
   }
 }
