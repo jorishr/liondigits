cbd0bee3956d2e93ae9927b324f00016e1692d11
jorishr <joris@liondigits.com>
2023-03-14 22:49:22 +0100

    Nav link moved from header to hero link section

--- app/scripts/modules/typewriter.js
+++ app/scripts/modules/typewriter.js
@@ -10,7 +10,7 @@ export function typewriterEffect() {
   });
 
   typewriter
-    .pauseFor(2000)
+    .pauseFor(1500)
     //.pasteString("<span style='font-size: var(--fs-xl);'>LION </span>")
     .typeString("<span>01101100 01101001</span>")
     .pauseFor(300)
