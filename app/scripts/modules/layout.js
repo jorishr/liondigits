2399ef91edfeb7c45dd01e47b1698658bf15245d
jorishr <joris@liondigits.com>
2023-12-14 20:07:58 +0100

    [BUG] Added condition, when element is not on page The js code may run on a page without an element layout.js is trying to access.

--- app/scripts/modules/layout.js
+++ app/scripts/modules/layout.js
@@ -1,16 +1,20 @@
 export function processHeaderHeight() {
   const header = document.querySelector(".header");
-  const headerHeight = header.offsetHeight;
-  document.documentElement.style.setProperty(
-    "--header-height",
-    `-${headerHeight}px`
-  );
+  if (header) {
+    const headerHeight = header.offsetHeight;
+    document.documentElement.style.setProperty(
+      "--header-height",
+      `-${headerHeight}px`
+    );
+  }
 }
 export function processMenuHeight() {
   const menu = document.querySelector(".skill__heading__nav");
-  const menuHeight = menu.offsetHeight;
-  document.documentElement.style.setProperty(
-    "--menu-height",
-    `${menuHeight}px`
-  );
+  if (menu) {
+    const menuHeight = menu.offsetHeight;
+    document.documentElement.style.setProperty(
+      "--menu-height",
+      `${menuHeight}px`
+    );
+  }
 }
