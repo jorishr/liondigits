3f2672771a60bdd95c6c85d1e835cfe3785fd69c
jorishr <joris@liondigits.com>
2024-01-03 20:17:25 +0100

    [FEAT] Adds snowfall.js animation
    - Adds modified snowfall.js script
    - Separates between own scripts and vendor scripts
    - Adds animation on/off toggle to the footer section

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,3 @@
-import { calFn } from "./modules/cal.js";
 import {
   langMenuToggle,
   langMenuOptions,
@@ -21,24 +20,8 @@ import {
   stickyNavOnScroll,
   navHighlightOnScroll,
 } from "./modules/scroll.js";
-import { typewriterEffect } from "./modules/typewriter.js";
 import consent from "./modules/consent.js";
-import AOS from "aos";
-import "aos/dist/aos.css";
 import setFooterCredits from "./modules/set-footer-credits.js";
-
-// Important: Make sure to add `data-cal-link="liondigits/20min"` attribute to the element you want to open Cal on click
-try {
-  calFn(window, "https://app.cal.com/embed/embed.js", "init");
-  Cal("init", { origin: "https://app.cal.com" });
-  Cal("ui", {
-    theme: "dark",
-    styles: { branding: { brandColor: "#003F8C" } },
-    hideEventTypeDetails: false,
-  });
-} catch (e) {
-  console.log("Calendar unable to loaded");
-}
 
 processElementHeight(".header", "--height-header");
 processElementHeight(".intro__highlight", "--height-highlights");
@@ -57,9 +40,7 @@ setAnchorLinks();
 setTxtContent();
 scrollIndicator();
 scrollDown();
+consent();
+setFooterCredits();
 stickyNavOnScroll();
 navHighlightOnScroll();
-typewriterEffect();
-consent();
-AOS.init();
-setFooterCredits();
